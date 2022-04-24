d5260097daaf1f2e1bd0e93d8ff6ca43d8747f6b
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-04-24 12:40:01 -0400

    Create currency input state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [input, setInput] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello world!</h1>
+      <form>
+        <input
+          className="currency-input"
+          type="number"
+          step="any"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
     </div>
   );
 }

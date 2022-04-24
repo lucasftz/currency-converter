21d8674467ebf6f1149695efb94b6a9a7bc9f75a
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-04-24 13:03:40 -0400

    Separate input form into its own component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import CurrencyForm from "./components/CurrencyForm";
 
 function App() {
   const [input, setInput] = useState(0);
@@ -6,15 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="bg-[#1E2027] text-neutral-300 p-5">
-        <form>
-          <input
-            className="border-[#2D2F3A] bg-transparent border-2 border-solid rounded-md p-2"
-            type="number"
-            step="any"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-          />
-        </form>
+        <CurrencyForm value={input} setValue={setInput} />
       </div>
     </div>
   );

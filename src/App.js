fb99861686d795db8e8665a182b9a0227a6ad3f2
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-04-24 13:39:32 -0400

    Refactors input form back into app component and set up onBlur

--- src/App.js
+++ src/App.js
@@ -1,13 +1,27 @@
 import { useState } from "react";
-import CurrencyForm from "./components/CurrencyForm";
 
 function App() {
   const [input, setInput] = useState(0);
 
+  const convertInput = () => {
+    // this will call the fixer API to convert the currency
+    console.log(`Input is ${input}`);
+  };
+
   return (
     <div className="App">
       <div className="bg-[#1E2027] text-neutral-300 p-5">
-        <CurrencyForm value={input} setValue={setInput} />
+        {/* input form */}
+        <form>
+          <input
+            className="border-[#2D2F3A] bg-transparent border-2 border-solid rounded-md p-2"
+            type="number"
+            step="any"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onBlur={convertInput}
+          />
+        </form>
       </div>
     </div>
   );

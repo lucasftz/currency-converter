3057701c37f8c8e85ed8eb47d8c3d8df3218425d
Lucas Fietkiewicz <lucas.fietkiewicz@gmail.com>
2022-04-24 15:00:54 -0400

    Get and display input converted from USD to PEN from fixer API

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import { useState } from "react";
+import { API_KEY } from "./secrets";
 
 function App() {
   const [input, setInput] = useState(0);
+  const [output, setOutput] = useState(0);
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("PEN");
 
-  const convertInput = () => {
-    // this will call the fixer API to convert the currency
-    console.log(`Input is ${input}`);
+  const convertInput = (e) => {
+    e.preventDefault();
+    fetch("http://data.fixer.io/api/latest" + `?access_key=${API_KEY}`)
+      .then((res) => res.json())
+      .then((data) => setOutput(input * (data.rates[to] / data.rates[from])));
   };
 
   return (
     <div className="App">
       <div className="bg-[#1E2027] text-neutral-300 p-5">
         {/* input form */}
-        <form>
+        <form onSubmit={convertInput}>
           <input
             className="border-[#2D2F3A] bg-transparent border-2 border-solid rounded-md p-2"
             type="number"
@@ -22,6 +28,7 @@ function App() {
             onBlur={convertInput}
           />
         </form>
+        <h1>{output.toFixed(2)}</h1>
       </div>
     </div>
   );
